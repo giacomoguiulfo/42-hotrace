4cec65e500a859da54b206013c25c99f938fa1b2
gguiulfo <gguiulfo@student.42.fr>
2017-05-14 16:19:34 -0700

    Reading input is working

--- src/hr_main.c
+++ src/hr_main.c
@@ -6,19 +6,51 @@
 /*   By: gguiulfo <gguiulfo@student.42.us.org>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/14 11:24:33 by gguiulfo          #+#    #+#             */
-/*   Updated: 2017/05/14 14:56:31 by gguiulfo         ###   ########.fr       */
+/*   Updated: 2017/05/14 16:19:04 by gguiulfo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <hotrace.h>
 
+// This will return a t_trie later
+void	hr_read(void)
+{
+	int		chr;
+	char	*key;
+	char	*value;
+
+	chr = 1;
+	while (chr)
+	{
+		get_next_line(0, &key);
+		chr = (key[0]) ? 1 : 0;
+		if (chr)
+		{
+			get_next_line(0, &value);
+			chr = (value[0]) ? 1 : 0;
+		}
+		if (chr)
+			; // store stuff
+	}
+}
+
+void	hr_query(void)
+{
+	int		chr;
+	char	*query;
+
+	chr = 1;
+	while (chr)
+	{
+		get_next_line(0, &query);
+		chr = (query[0]) ? 1 : 0;
+		// print query value for key
+	}
+}
+
 int		main(void)
 {
-	char *line;
-
-	while (get_next_line(0, &line) > 0)
-	{
-		ft_putstr(line);
-	}
-	return (0);
+	hr_read();
+	hr_query();
+	// hr_dlt_trie();
 }
