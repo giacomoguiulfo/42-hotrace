bf0927311c8d6f5b3a3bedcdd83af448a258b1b9
Akinfemi Akin-Aluko <aakin-al@e1z2r8p15.42.us.org>
2017-05-14 15:08:24 -0700

    init radix tree

--- includes/hotrace.h
+++ includes/hotrace.h
@@ -6,7 +6,7 @@
 /*   By: gguiulfo <gguiulfo@student.42.us.org>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/14 11:24:17 by gguiulfo          #+#    #+#             */
-/*   Updated: 2017/05/14 13:59:49 by gguiulfo         ###   ########.fr       */
+/*   Updated: 2017/05/14 15:07:29 by aakin-al         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,4 +15,13 @@
 
 # include "ft_utils.h"
 
+typedef struct				s_radix_tree
+{
+	struct s_radix_tree		*left;
+	struct s_radix_tree		*right;
+	char					*value;
+}t_radix_tree;
+t_radix_tree				*hr_new_node(char *str);
+void						hr_addnode(t_radix_tree *node, t_radix_tree *root);
+t_radix_tree				*hr_getnode(char *str, t_radix_tree *root);
 #endif
